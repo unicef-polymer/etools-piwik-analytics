87191c3fa82b461da39be8cb376215f3d243c3f4
drzackyll <adams.z.d@gmail.com>
2020-05-07 15:40:36 -0400

    Reverts previous commit

--- etools-piwik-analytics.js
+++ etools-piwik-analytics.js
@@ -25,7 +25,7 @@ if (!('_paq' in window)) {
   window._paq = [];
 }
 // disables tracking for local development
-if (window.location.host.indexOf('localhost') > -1) {
+if (SITE_ID === '6') {
   _paq.push(['requireConsent']);
 }
 _paq.push(['trackPageView']);
